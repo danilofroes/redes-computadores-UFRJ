69bc768fcaafdb5cd8fd4d4a499868a855b3ab66
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2026-05-19 23:28:17 -0300

    ADD: main padrão para espidf

--- estacao-telemetria-lora/src/transmissor/main.cpp
+++ estacao-telemetria-lora/src/transmissor/main.cpp
@@ -9,17 +9,51 @@
  * @date 2026
  */
 
- #include "freertos/FreeRTOS.h"
- #include "freertos/task.h"
+#include <iostream>
 
- extern "C" {
-    void app_main(void);
- }
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
 
- void app_main(void) {
+#include "../include/sensores/ldr.hpp"
 
-    while (true) {
+static const char* TAG = "Transmissor";
 
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
- }
+extern "C" {
+   void app_main(void);
+}
+
+void setup();
+void loop();
+
+void app_main(void) {
+   setup();
+
+   while (true) {
+      loop();
+      vTaskDelay(pdMS_TO_TICKS(10));
+   }
+}
+
+void setup() {
+   // Inicializando a non-volatile storage (NVS)
+   esp_err_t ret = nvs_flash_init();
+
+   // Verificando se a inicialização da NVS falhou
+   if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+         ESP_LOGW(TAG, "Problema na NVS, corrigindo...");
+
+         ESP_ERROR_CHECK(nvs_flash_erase()); // Limpando a NVS
+         ret = nvs_flash_init(); // Tentando inicializar novamente
+   }
+   
+   ESP_ERROR_CHECK(ret); // Verificando se a inicialização da NVS foi bem-sucedida
+
+   ESP_LOGI(TAG, "Iniciando Estação de Telemetria LoRa...");
+}
+
+void loop() {
+   ESP_LOGI(TAG, "Lendo sensores...");
+}
